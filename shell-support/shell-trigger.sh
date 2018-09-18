b56a5e9e3b84df8741470fd572f49fb93e61b1f8
Clemens Fruhwirth <clemens@endorphin.org>
2018-09-18 17:17:15 +0200

    Kick gpg signing from shell-trigger. This is an overkill ATM.

--- shell-support/shell-trigger.sh
+++ shell-support/shell-trigger.sh
@@ -1,42 +1,21 @@
-verify_signature() {
-    local file=$1 sigfile=$2 trusted=$3 out=
-    if out=$(gpg2 --status-fd 1 --verify $sigfile $file 2>/dev/null) &&
-       echo "$out" | grep -qs "^\[GNUPG:\] VALIDSIG $trusted " &&
-       echo "$out" | grep -qs "^\[GNUPG:\] TRUST_ULTIMATE"; then
-	return 0
-    else
-	echo "$out" >&2
-	return 1
-    fi
-}
-
 chpwd() {
     if [ -n "$TRUSTED_KEY" ]; then
-      SHELL_NIX=$(findup shell.nix)
-      if [ -n "$SHELL_NIX" ]; then
-	  if [ -e "$SHELL_NIX".sig ]; then
-	      if verify_signature $SHELL_NIX $SHELL_NIX.sig $TRUSTED_KEY; then
-		  echo "good sig found."
-		  # The protocol between this helper and the shell spawned is that
-		  # this shell will chdir to a dir written to file descriptor 3
-		  # by nix-shell or its subprocesses.
-		  CAGEFILE=$(mktemp /tmp/cage.XXXXXX)
-		  exec 3> $CAGEFILE
+      JAIL_NIX=$(findup jail.nix)
+      if [ -n "$JAIL_NIX" ]; then
+	  # The protocol between this helper and the shell spawned is that
+	  # this shell will chdir to a dir written to file descriptor 3
+	  # by nix-shell or its subprocesses.
+	  CAGEFILE=$(mktemp /tmp/cage.XXXXXX)
+	  exec 3> $CAGEFILE
 
-		  nix-shell $SHELL_NIX
-		  NEWDIR=$(cat $CAGEFILE)
-		  rm $CAGEFILE
-		  if [ -n "$NEWDIR" ]; then
-		     cd $NEWDIR
-		  else
-		      echo "Returning to original dir."
-		      cd $OLDPWD
-		  fi
-	      else
-		  echo "signature verification failed. NOT IN SHELL".
-	      fi
+	  nix-shell $JAIL_NIX
+	  NEWDIR=$(cat $CAGEFILE)
+	  rm $CAGEFILE
+	  if [ -n "$NEWDIR" ]; then
+	      cd $NEWDIR
 	  else
-	      echo "shell.nix found but no shell.nix.sig."
+	      echo "pwdjail-nix: Sub-shell quit without giving a target directory. Returning you to $OLDPWD."
+	      cd $OLDPWD
 	  fi
       fi
    fi
